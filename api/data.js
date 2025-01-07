e0f147716397ddd3c4054aecd713914ec3619555
ghozi29 <ghoziskripsi@gmail.com>
2025-01-07 14:40:30 +0700

    Update data.js

--- api/data.js
+++ api/data.js
@@ -33,3 +33,9 @@ app.post('/api/data', (req, res) => {
 });
 
 module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
