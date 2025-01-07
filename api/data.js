05d4f132a4a307e431cb153422a2444ed569b7c9
ghozi2908 <ghoziyusuf1@gmail.com>
2025-01-07 14:25:46 +0700

    first commit

---
+++ api/data.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const app = express();
+app.use(express.json());  // Middleware untuk parsing JSON
+
+// Inisialisasi Firebase Admin SDK
+const serviceAccount = require('../config/serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://kesehatanwajah-ea5dd-default-rtdb.firebaseio.com"
+});
+
+const db = admin.database();
+const ref = db.ref("sensorData");
+
+app.post('/api/data', (req, res) => {
+  console.log("Received data:", req.body);
+  const data = req.body;
+  const newData = ref.push();
+  newData.set({
+    suhu: data.suhu,
+    kelembapan: data.kelembapan,
+    tinggi: data.tinggi,
+    timestamp: Date.now()
+  })
+  .then(() => {
+    res.status(200).send('Data saved successfully!');
+  })
+  .catch((error) => {
+    console.error("Error saving data:", error);
+    res.status(500).send('Error saving data: ' + error.message);
+  });
+});
+
+module.exports = app;
